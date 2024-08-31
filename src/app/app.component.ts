e7ccbbc7381defa9abf2095af5a666f670d8ef2f
Alice <alicetluc@gmail.com>
2024-08-31 17:25:55 -0400

    style nav-bar/nav-bar-alternative and add home-page component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+
 import { NavBarComponent } from "./nav-bar/nav-bar.component";
+import { NavBarAlternativeComponent } from "./nav-bar-alternative/nav-bar-alternative.component";
+import { HomePageComponent } from "./home-page/home-page.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavBarComponent],
+  imports: [RouterOutlet, NavBarComponent, NavBarAlternativeComponent, HomePageComponent,],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
---
+++ src/app/nav-bar-alternative/nav-bar-alternative.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-nav-bar-alternative',
+  standalone: true,
+  imports: [],
+  templateUrl: './nav-bar-alternative.component.html',
+  styleUrl: './nav-bar-alternative.component.scss'
+})
+export class NavBarAlternativeComponent {
+
+}
