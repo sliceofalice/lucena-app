291656119644ea80dcea8996c1be9e5c2dd5834e
Alice Teles Lucena <aliceluc@motorola.com>
2024-09-16 11:12:11 -0400

    configure translateModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { BrowserModule } from '@angular/platform-browser';
-import { TranslateModule } from '@ngx-translate/core';
+import { HttpClient } from '@angular/common/http';
+import { HttpClientModule } from '@angular/common/http';
+
+import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
+import {TranslateHttpLoader} from '@ngx-translate/http-loader';
 
 import { AppComponent } from './app.component';
 import { SharedModule } from './shared/shared.module';
 import { HomePageModule } from './home-page/home-page.module';
+
+export function createTranslateLoader(http: HttpClient) {
+  return new TranslateHttpLoader(http, './assets/i18n/', '.json');
+}
 
 @NgModule({
   bootstrap: [AppComponent],
@@ -15,7 +23,17 @@ import { HomePageModule } from './home-page/home-page.module';
   imports: [
     BrowserModule,
     CommonModule,
-    TranslateModule.forRoot(),
+    HttpClientModule,
+    TranslateModule.forRoot(
+      {
+        defaultLanguage: 'en',
+        loader: {
+          provide: TranslateLoader,
+          useFactory: (createTranslateLoader),
+          deps: [HttpClient]
+        }
+      }
+    ),
     SharedModule,
     HomePageModule,
   ]
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,9 +1,12 @@
 import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { TranslateModule } from '@ngx-translate/core';
 
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 
 @NgModule({
   declarations: [NavBarComponent],
   exports: [NavBarComponent],
+  imports: [CommonModule, TranslateModule]
 })
 export class SharedModule { }
