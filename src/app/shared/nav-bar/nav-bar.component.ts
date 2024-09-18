8a2267c94d36ce257549ad04afafe6ebdf262cec
Alice Teles Lucena <aliceluc@motorola.com>
2024-09-18 07:33:32 -0400

    resolve duplicates

--- src/app/shared/nav-bar/nav-bar.component.ts
+++ src/app/shared/nav-bar/nav-bar.component.ts
@@ -9,23 +9,10 @@ export class NavBarComponent {
   @Input() type?: string;
   @Input() languageClicked?: boolean;
 
-  @Input() type?: string;
-  @Input() languageClicked?: boolean;
-
   profiles = ['Alice', 'Ketlen', 'Walfredo'];
   languageDropdown = false;
 
   get navBarType() : string {
-    return this.type ? `nav-bar-default ${this.type}` : 'nav-bar-default';
-  }
-
-  get languageClass() : string {
-    return this.languageClicked ? 'language-default language-clicked poppins-light' : 'language-default poppins-light';
-  }
-
-  onClickLanguage() : void{
-    this.languageClicked = !this.languageClicked;
-    this.languageDropdown = !this.languageDropdown;
     return this.type ? `nav-bar-default ${this.type}` : 'nav-bar-default';
   }
 
